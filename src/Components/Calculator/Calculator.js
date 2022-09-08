0215541d6f61ed9273fe173246f2718d85c98c37
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-09-08 16:46:04 +0100

    refactor Calculator.js file to use react hooks and add minor styling effects for users

--- src/Components/Calculator/Calculator.js
+++ src/Components/Calculator/Calculator.js
@@ -1,69 +1,64 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import calculate from '../../logic/calculate';
 import './calculator.scss';
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: '',
-      operation: '',
-    };
-  }
+const Calculator = () => {
+  const [state, setState] = useState({
+    total: 0,
+    next: '',
+    operation: '',
+  });
 
-  handleClick(e) {
+  const handleClick = (e) => {
     const text = e.target.textContent;
-    const result = calculate(this.state, text);
-    return this.setState(result);
-  }
+    const result = calculate(state, text);
+    return setState(result);
+  };
 
-  render() {
-    const { total, next, operation } = this.state;
-    return (
-      <>
-        <div className="calculator-container">
-          <div className="answer-div">
-            <span>
-              {total}
+  const { total, next, operation } = state;
+  return (
+    <>
+      <div className="calculator-container">
+        <div className="answer-div">
+          <span>
+            {total}
 
-              {operation}
+            {operation}
 
-              {next}
-            </span>
-          </div>
-          <div className="first-div">
-            <button type="button" className="btn clear-all number" onClick={this.handleClick.bind(this)}>AC</button>
-            <button type="button" className="btn number" onClick={this.handleClick.bind(this)}>+/-</button>
-            <button type="button" className="btn modulus number" onClick={this.handleClick.bind(this)}>%</button>
-            <button type="button" className="btn divide operator" onClick={this.handleClick.bind(this)}>÷</button>
-          </div>
-          <div className="second-div">
-            <button type="button" className="btn number" onClick={this.handleClick.bind(this)}>7</button>
-            <button type="button" className="btn number" onClick={this.handleClick.bind(this)}>8</button>
-            <button type="button" className="btn number" onClick={this.handleClick.bind(this)}>9</button>
-            <button type="button" className="btn multiplication operator" onClick={this.handleClick.bind(this)}>x</button>
-          </div>
-          <div className="third-div">
-            <button type="button" className="btn number" onClick={this.handleClick.bind(this)}>4</button>
-            <button type="button" className="btn number" onClick={this.handleClick.bind(this)}>5</button>
-            <button type="button" className="btn number" onClick={this.handleClick.bind(this)}>6</button>
-            <button type="button" className="btn substraction operator" onClick={this.handleClick.bind(this)}>-</button>
-          </div>
-          <div className="fourth-div">
-            <button type="button" className="btn number" onClick={this.handleClick.bind(this)}>1</button>
-            <button type="button" className="btn number" onClick={this.handleClick.bind(this)}>2</button>
-            <button type="button" className="btn number" onClick={this.handleClick.bind(this)}>3</button>
-            <button type="button" className="btn addition operator" onClick={this.handleClick.bind(this)}>+</button>
-          </div>
-          <div className="fourth-div">
-            <button type="button" className="btn number zero" onClick={this.handleClick.bind(this)}>0</button>
-            <button type="button" className="btn period number" onClick={this.handleClick.bind(this)}>.</button>
-            <button type="button" className="btn equals-to operator" onClick={this.handleClick.bind(this)}>=</button>
-          </div>
+            {next}
+          </span>
         </div>
-      </>
-    );
-  }
-}
+        <div className="first-div">
+          <button type="button" className="btn clear-all number" onClick={handleClick}>AC</button>
+          <button type="button" className="btn number" onClick={handleClick}>+/-</button>
+          <button type="button" className="btn modulus number" onClick={handleClick}>%</button>
+          <button type="button" className="btn divide operator" onClick={handleClick}>÷</button>
+        </div>
+        <div className="second-div">
+          <button type="button" className="btn number" onClick={handleClick}>7</button>
+          <button type="button" className="btn number" onClick={handleClick}>8</button>
+          <button type="button" className="btn number" onClick={handleClick}>9</button>
+          <button type="button" className="btn multiplication operator" onClick={handleClick}>x</button>
+        </div>
+        <div className="third-div">
+          <button type="button" className="btn number" onClick={handleClick}>4</button>
+          <button type="button" className="btn number" onClick={handleClick}>5</button>
+          <button type="button" className="btn number" onClick={handleClick}>6</button>
+          <button type="button" className="btn substraction operator" onClick={handleClick}>-</button>
+        </div>
+        <div className="fourth-div">
+          <button type="button" className="btn number" onClick={handleClick}>1</button>
+          <button type="button" className="btn number" onClick={handleClick}>2</button>
+          <button type="button" className="btn number" onClick={handleClick}>3</button>
+          <button type="button" className="btn addition operator" onClick={handleClick}>+</button>
+        </div>
+        <div className="fourth-div">
+          <button type="button" className="btn number zero" onClick={handleClick}>0</button>
+          <button type="button" className="btn period number" onClick={handleClick}>.</button>
+          <button type="button" className="btn equals-to operator" onClick={handleClick}>=</button>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Calculator;
